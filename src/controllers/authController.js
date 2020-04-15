beba2e27d4dcb69a70a0ce777f766b085e77456d
Murilo Victor <murilovictor63@gmail.com>
2020-04-15 19:00:08 -0300

    criptografando senha com bcrypt

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,13 +3,23 @@ const User = require('../models/User')
 const router = express.Router()
 
 router.post('/register', async (req, res) => {
-    console.log('body: ', req.body)
+    const { email } = req.body;
+
     try {
+
+        if(await User.findOne({ email })){
+            return res.status(400).send({ error: 'User already exists.' })
+        }
+
         const user = await User.create(req.body);
+        
+        // remove password
+        user.password = undefined;
+
         return res.send({ user })
     } catch (err) {
-        console.log(err)
-        return res.status(400).send({error: 'Registration failed. ' + err.errmsg})     
+        console.error(err)
+        return res.status(400).send({ error: 'Registration failed.' })     
     }
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,18 +1,18 @@
-const mongoose = require('./../database'); // Erase if already required
+const mongoose = require('./../database')
+const bcrypt = require('bcryptjs') // Erase if already required
 
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
     name:{
         type:String,
         required:true,
-        unique:true,
-        index:true,
     },
     email:{
         type:String,
         required:true,
         unique:true,
-        lowercase: true
+        lowercase: true,
+        index:true,
     },
     password:{
         type:String,
@@ -25,6 +25,11 @@ var userSchema = new mongoose.Schema({
     }
 });
 
-//Export the model
+userSchema.pre('save', async function(next) {
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash;
+    next();
+})
 
+//Export the model
 module.exports = mongoose.model('User', userSchema);
